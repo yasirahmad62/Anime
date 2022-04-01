05de0a792552f0f6d177bb41151530439c4fc910
yasirahmad62 <yasir120916@gmail.com>
2022-04-01 19:29:43 +0530

    fix

--- src/features/movies/movieSlice.js
+++ src/features/movies/movieSlice.js
@@ -1,6 +1,5 @@
 import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 import movieApi from "../../common/apis/movieApi";
-import { APIKey } from "../../common/apis/MovieApiKey";
 
 export const fetchAsyncMovies = createAsyncThunk(
   "movies/fetchAsyncMovies",
