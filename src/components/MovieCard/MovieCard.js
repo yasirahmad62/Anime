1d8d600104a4e2db0f40e85af6e0ff9372987353
yasirahmad62 <yasir120916@gmail.com>
2022-04-11 15:28:32 +0530

    title style fix

--- src/components/MovieCard/MovieCard.js
+++ src/components/MovieCard/MovieCard.js
@@ -6,16 +6,16 @@ const MovieCard = (props) => {
   const { data } = props;
   return (
     <div className="card-item">
-        <div className="card-inner">
-          <div className="card-top">
-            <img src={data.image_url} alt={data.title} />
-          </div>
-          <div className="card-bottom">
-            <div className="card-info">
-              <h4>{data.title}</h4>
-            </div>
+      <div className="card-inner">
+        <div className="card-top">
+          <img src={data.image_url} alt={data.title} />
+        </div>
+        <div className="card-bottom">
+          <div className="card-info">
+            <h4>{data.title}</h4>
           </div>
         </div>
+      </div>
     </div>
   );
 };
